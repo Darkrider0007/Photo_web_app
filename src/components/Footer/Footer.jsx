63c03a1d6e0dbd6f09dcb9fcd99dd8d962a04098
Rohan Gope <rg2512@it.jgec.ac.in>
2023-12-03 21:43:08 +0530

    Update GitHub repo link in Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
     <div className='w-full bg-black/10'>
       <div className='flex flex-col items-center justify-center py-4'>
         <p className='text-gray-500'>© 2023 Rohan Gope. All rights reserved.</p>
-       <Link to='#' className='text-gray-500 hover:text-gray-600 transition duration-300 cursor-pointer'>git hub repo</Link>
+       <Link to='https://github.com/Darkrider0007/Photo_web_app' className='text-gray-500 hover:text-gray-600 transition duration-300 cursor-pointer'>git hub repo</Link>
       </div>
     </div>
   );
